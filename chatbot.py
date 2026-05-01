cc19ddca329bc6acadec88bc1d873de259423ca7
Reed0124 <792936825@qq.com>
2026-05-01 19:47:31 +0800

    Call up large model testing

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,13 @@
+from platform import system
+
 import streamlit as st
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    api_key=os.environ.get('DEEPSEEK_API_KEY'),
+    base_url="https://api.deepseek.com"
+)
 
 st.set_page_config(
     page_title="ChatBot",
@@ -11,14 +20,34 @@ st.set_page_config(
 # 大标题
 st.title("ChatBot")
 
+# 存储聊天记录
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages: #{"role": "user", "content": prompt}
+    st.chat_message(message["role"]).write(message["content"])
+
+system_prompt = "你是一个雅思英语学习助手"
+
 # 聊天框
 prompt = st.chat_input("Your message")
 if prompt:
     st.chat_message("user").write(prompt)
-    st.chat_message("assistant").write("正在思考...")
-    print("调用大模型，提示词：", prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # 调用大模型，获取回复
+    # 调用大模型
+    response = client.chat.completions.create(
+        model="deepseek-v4-pro",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": prompt},
+        ],
+        stream=False,
+        reasoning_effort="high",
+        extra_body={"thinking": {"type": "enabled"}}
+    )
+    st.chat_message("assistant").write(response.choices[0].message.content)
+    st.session_state.messages.append({"role": "assistant", "content": response.choices[0].message.content})
 
 # prompt = st.chat_input(
 #     placeholder="输入消息并可上传图片...",
